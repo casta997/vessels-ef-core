63d8f939310f820ef719fed965459f16f2ea34ac
Raul William Fernanz Pampa (MSC Technology Italia) <raulwilliam.fernanzpampa@msc.com>
2025-02-24 09:35:18 +0100

    add commands for operations

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -5,15 +5,60 @@ using Application.ManageProgram;
 
 var manageProgram = new OperationsCentralBase();
 
-//manageProgram.AddVessel();
-//manageProgram.ShowVessels();
-//manageProgram.UpdateVessel();
-//manageProgram.DeleteVessel();
+var isProgramOn = true;
 
+while (isProgramOn)
+{
+    Console.WriteLine(@"
+---------------------Managing Vessels---------------------
+Select 1 operation:
+CV.- Create a new vessel.
+CO.- Create a new owner.
+RV.- Display the list of vessels.
+RO.- Display the list of owners.
+UV.- Update the details of an existing vessel.
+UO.- Update the details of an existing owner.
+DV.- Delete a vessel.
+DO.- Delete an owner.
+A.- Assign a vessel to an owner.
+");
 
-manageProgram.AddOwner();
-//manageProgram.ShowOwners();
-//manageProgram.UpdateOwner();
-//manageProgram.DeleteOwner();
+    string inputTypeOperation = Console.ReadLine();
+    Console.Clear();
 
-//manageProgram.AssignVesselToOwner();
+    switch (inputTypeOperation)
+    {
+        case "CV":
+            manageProgram.AddVessel();
+            break;
+        case "CO":
+            manageProgram.AddOwner();
+            break;
+        case "RV":
+            manageProgram.ShowVessels();
+            break;
+        case "RO":
+            manageProgram.ShowOwners();
+            break;
+        case "UV":
+            manageProgram.UpdateVessel();
+            break;
+        case "UO":
+            manageProgram.UpdateOwner();
+            break;
+        case "DV":
+            manageProgram.DeleteVessel();
+            break;
+        case "DO":
+            manageProgram.DeleteOwner();
+            break;
+        case "A":
+            manageProgram.AssignVesselToOwner();
+            break;
+        default:
+            isProgramOn = false;
+            Console.WriteLine("Option not available!!\ntry again...");
+            break;
+    }
+}
+
