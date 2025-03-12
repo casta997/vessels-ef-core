dfdddc53c24bc7631c5644299dc6bf8af227541c
wi11iamFernandez <william.fernandez@edu.itspiemonte.it>
2025-03-12 12:05:37 +0100

    clone project asp.net core web api

---
+++ src/CarRentalApplication/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalApplication.Controllers
+{
+    [ApiController]
+    [Route("cars")]
+    public class CarController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{carId:long}")]
+        public IActionResult GetById(long carId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/CarRentalApplication/Controllers/RentalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalApplication.Controllers
+{
+    [ApiController]
+    [Route("rentals")]
+    public class RentalController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{rentalId:long}")]
+        public IActionResult GetById(long rentalId)
+        {
+            return Ok();
+        }
+    }
+}
